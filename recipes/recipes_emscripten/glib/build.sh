272139d01dfed4853b082a5751f888ac69dd7598
Isabel Paredes <isabel.paredes@quantstack.net>
2024-06-21 12:09:11 +0200

    Update recipe

--- recipes/recipes_emscripten/glib/build.sh
+++ recipes/recipes_emscripten/glib/build.sh
@@ -1,9 +1,36 @@
-emconfigure ./configure \
-    --enable-shared=no \
-    --enable-static=yes \
-    --with-threads=no \
-    --with-pcre=internal \
-    --disable-libmount \
-    --prefix=${PREFIX}
+# emconfigure ./configure \
+#     --enable-shared=no \
+#     --enable-static=yes \
+#     --with-threads=no \
+#     --with-pcre=internal \
+#     --disable-libmount \
+#     --prefix=${PREFIX}
 
-emmake make install
+# emmake make install
+
+
+meson_config_args=(
+    -Dlibdir=lib
+    -Dlibmount=disabled
+    -Dselinux=disabled
+    -Dlibelf=disabled
+    -Dxattr=false
+    -Dtests=false
+)
+
+# meson --buildtype=release --prefix="$PREFIX" --backend=ninja -Dlibdir=lib \
+#       -Diconv=external -Dlibmount=disabled -Dselinux=disabled -Dxattr=false ..
+# ninja -j${CPU_COUNT} -v
+
+
+meson setup builddir \
+    ${MESON_ARGS} \
+    "${meson_config_args[@]}" \
+    --buildtype=release \
+    --backend=ninja \
+    --default-library=static \
+    --prefix=$PREFIX \
+    --wrap-mode=nofallback \
+    --cross-file=$RECIPE_DIR/emscripten.meson.cross
+ninja -v -C builddir -j ${CPU_COUNT}
+ninja -C builddir install -j ${CPU_COUNT}
