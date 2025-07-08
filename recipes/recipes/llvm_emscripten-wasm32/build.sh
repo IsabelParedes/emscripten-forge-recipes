bd0ced10399424c666f56547964ffcede0b50558
Isabel Paredes <isabel.paredes@quantstack.net>
2025-07-08 12:22:01 +0200

    Add llvm and flang v19

---
+++ recipes/recipes/llvm_emscripten-wasm32/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eux
+
+export BUILDDIR="_build_flang_llvm"
+
+echo "🧿🧿🧿🧿🧿 CMaking"
+cmake -S llvm -B $BUILDDIR -GNinja \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX=$PREFIX \
+        -DCMAKE_PREFIX_PATH=$PREFIX \
+        -DCMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
+        -DLLVM_DEFAULT_TARGET_TRIPLE=wasm32-unknown-emscripten \
+        -DLLVM_TARGETS_TO_BUILD=WebAssembly \
+        -DLLVM_ENABLE_PROJECTS="clang;flang;mlir" \
+        -DLLVM_BUILTIN_TARGETS="wasm32-unknown-emscripten" \
+        -DCMAKE_CFLAGS="" \
+        -DCMAKE_CXXFLAGS="" \
+        -DCMAKE_VERBOSE_MAKEFILE=ON
+
+echo "♥️♥️♥️♥️♥️ Building"
+cmake --build $BUILDDIR -j6
+
+echo "🔫🔫🔫🔫🔫 Installing"
+cmake --build $BUILDDIR --target install
